ab963dedcff34b3a4f4e91d824cb2c649894ea02
ttricker <ttricker@gmail.com>
2012-11-21 14:00:46 -0800

    Remove l1 & l2 files

--- src/cache.c
+++ src/cache.c
@@ -5,18 +5,18 @@
 /* Allocates a new cache object with the following parameters
  *
  */
-struct cache* cache_new(size_t associativity, 
+struct cache_t* cache_new(size_t associativity, 
 			size_t line_size, 
 			size_t index_size,
 			struct LN_ops_t interface)
 {
-    struct cache* new_cache = malloc(sizeof(struct cache) + 
+    struct cache_t* new_cache = malloc(sizeof(struct cache_t) + 
 				     index_size*sizeof(struct set_t*));
     if(!new_cache) return NULL; //watch for malloc errors
     //Set Parameter structure
-    new_cache->cache_params.associativity = associativity;
-    new_cache->cache_params.line_size = line_size;
-    new_cache->cache_params.index_size = index_size;
+    new_cache->params.associativity = associativity;
+    new_cache->params.line_size = line_size;
+    new_cache->params.index_size = index_size;
 
     //Allocate Sets
     for(int i = 0; i < index_size; ++i)
@@ -28,19 +28,20 @@ struct cache* cache_new(size_t associativity,
 	    new_cache->set[i]->line[j] = malloc(sizeof(struct line_t) + 
 						line_size*sizeof(uint8_t));
     }
+    cache_reset(new_cache);
     return new_cache;
     //TODO add clean up code if malloc fails in one of the loops
 }
 
 /* deallocate the memory used by the cache obj */
-void cache_free(struct cache* cacheobj)
+void cache_free(struct cache_t* cacheobj)
 {
     //Allow NULL free
     if(!cacheobj) return;
 
     for(int i = 0; i < cacheobj->params.index_size; ++i)
     {
-	for(int j = 0; i < cacheobj->params.associativity)
+	for(int j = 0; j < cacheobj->params.associativity; ++j)
 	{
 	    free(cacheobj->set[i]->line[j]); //free everyline in every set
 	}
@@ -49,3 +50,8 @@ void cache_free(struct cache* cacheobj)
     free(cacheobj);
 }
 
+/* Clear Status bits and setup check LRU */
+void cache_reset(struct cache_t* cacheobj)
+{
+    
+}
--- src/cache.h
+++ src/cache.h
@@ -9,12 +9,6 @@
  */
 struct status_t
 {
-	COMMAND_READ 		= 0,
-	COMMAND_WRITE 		= 1,
-	COMMAND_IFETCH 		= 2,		// read request from L1
-	COMMAND_INVALIDATE 	= 3,	// comes from L2
-	COMMAND_RESET 		= 8,	
-	COMMAND_PRINT 		= 9
     unsigned valid:1;
     unsigned dirty:1;
 };
@@ -23,15 +17,13 @@ struct status_t
 struct line_t
 {
     uint32_t tag;               // cache tag
-    status_t status;            // dirty and valid bits
+    struct status_t status;            // dirty and valid bits
     uint8_t  data[];	        // line data
 };
 
 // Set structure
 struct set_t
 {
-    uint32_t *lru;			// LRU bits:
-    //int lrusize;			// number of bits in LRU
     uint8_t *lru;			// LRU bits:
     int lrusize;			// number of bits in LRU
 					// Space complexity: N*log2(N)
@@ -54,7 +46,7 @@ struct LN_ops_t
     op_status_t (*modified)(uint32_t address, size_t length, uint8_t* data);
 };
 
-struct cache_params
+struct cache_params_t
 {
     size_t associativity;
     size_t line_size;
@@ -63,8 +55,10 @@ struct cache_params
 
 struct cache_statistics_t
 {
-    unsigned valid:1;
-    unsigned dirty:1;
+    size_t hits;
+    size_t misses;
+    size_t reads;
+    size_t writes;
 };
 
 /* Main cache data structure */
@@ -74,8 +68,6 @@ struct cache_t
     int setsize;     // number of sets
     int linesize;    // number of lines per set
     size_t data_size;// number of bytes in data
-    struct LN_ops_t ln_ops;     //callbacks to the lower memory hierarchy
-    struct set_t *set[];        //
     struct cache_statistics_t stats;
     struct cache_params_t     params; //size parameters of the cache
     struct LN_ops_t           ln_ops; //callbacks to the lower memory hierarchy
@@ -88,20 +80,20 @@ struct cache_t
  * associativity = number of lines per set 
  */
 
-struct cache* cache_new(size_t associativity, 
+struct cache_t* cache_new(size_t associativity, 
 			size_t line_size, 
 			size_t index_size,
 			struct LN_ops_t interface);
-void cache_free(struct cache* cacheobj);
-void cache_reset(struct cache* cacheobj);
+void cache_free(struct cache_t* cacheobj);
+void cache_reset(struct cache_t* cacheobj);
 
 // input operations
-void cache_readop(struct cache* cacheobj, uint32_t address);
-void cache_writeop(struct cache* cacheobj, uint32_t address);
-void cache_invalidate(struct cache* cacheobj, uint32_t address);
+void cache_readop(struct cache_t* cacheobj, uint32_t address);
+void cache_writeop(struct cache_t* cacheobj, uint32_t address);
+void cache_invalidate(struct cache_t* cacheobj, uint32_t address);
 
 // debuging functions
-void cache_print(const struct cache* cacheobj);
+void cache_print(const struct cache_t* cacheobj);
 
 #endif //__CACHE_H__
 
