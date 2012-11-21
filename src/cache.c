39b389e4b750e3ea7d21ee381152c41c2d971733
ttricker <ttricker@gmail.com>
2012-11-21 12:45:24 -0800

    Modified cache api to include a modify signal

--- src/cache.c
+++ src/cache.c
@@ -1,21 +1,110 @@
-#include "cache.h"
 #include <errno.h>
+#include <stdlib.h>
+#include "cache.h"
 
 
-/* 
+/* Allocates a new cache object with the following parameters
  *
  */
-struct cache* cache_new(size_t associativity, size_t line_size, size_t index_size)
+struct cache* cache_new(size_t associativity, 
+			size_t line_size, 
+			size_t index_size,
+			struct LN_ops_t interface)
 {
-    return NULL;
-}
-// LRU functions
+    struct cache* new_cache = malloc(sizeof(struct cache) + 
+				     index_size*sizeof(struct set_t*));
+    if(!new_cache) return NULL; //watch for malloc errors
+    //Set Parameter structure
+    new_cache->cache_params.associativity = associativity;
+    new_cache->cache_params.line_size = line_size;
+    new_cache->cache_params.index_size = index_size;
 
+    //Allocate Sets
+    for(int i = 0; i < index_size; ++i)
+    {
+	new_cache->set[i] = malloc(sizeof(struct set_t) +
+				   line_size*sizeof(struct line_t*));
+	//Allocate Lines + data
+	for(int j = 0; j < associativity; ++j)
+	    new_cache->set[i]->line[j] = malloc(sizeof(struct line_t) + 
+						line_size*sizeof(uint8_t));
+    }
+    return new_cache;
+    //TODO add clean up code if malloc fails in one of the loops
+}
+
+/* deallocate the memory used by the cache obj */
 void cache_free(struct cache* cacheobj)
 {
-    //stub
+    //Allow NULL free
+    if(!cacheobj) return;
+
+    for(int i = 0; i < cacheobj->params.index_size; ++i)
+    {
+	for(int j = 0; i < cacheobj->params.associativity)
+	{
+	    free(cacheobj->set[i]->line[j]); //free everyline in every set
+	}
+	free(cacheobj->set[i]); //free everyset
+    }
+    free(cacheobj);
 }
 
+<<<<<<< Updated upstream
+=======
+
+
+uint32_t touchLRU(struct set_t *set, uint32_t way)
+{
+	// update LRU 
+	int i;
+	uint32_t pivot = set->lru[way];
+
+	for (i=0; i<set->linesize; i++)
+	{
+		if (set->lru[i] < pivot)
+			set->lru[i]++;
+		else if (set->lru[i] == pivot)
+			set->lru[i] = 0x0;
+	}
+
+	return 0;
+}
+
+
+uint32_t findLRU(struct set_t *set)
+{
+	// return the least recently used way in a set
+	int i;
+
+	for (i=0; i<set->linesize; i++)
+	{
+		if (set->lru[i] == (set->linesize - 1))
+			return i;
+	}
+	return 0;
+}
+
+
+void invalidateLRUWay(struct set_t *set, uint32_t way)
+{
+	// set LRU way to least recently used
+
+	int i;
+	uint32_t pivot = set->lru[way];
+
+	for (i=0; i<set->linesize; i++)
+	{
+		if (i == way)
+			set->lru[i] = set->linesize - 1;
+		else if (set->lru[i] > pivot)
+			set->lru[i]--;
+	}
+
+	return;
+}
+
+>>>>>>> Stashed changes
 
 // Address Decomposition
 
@@ -57,6 +146,7 @@ void printStats(struct cache_t *cache, const char *name)
 
 	return;
 }
+<<<<<<< Updated upstream
 
 
 /*
@@ -172,3 +262,5 @@ void set_destroy(struct set_t *set)
 	return;
 }
 */
+=======
+>>>>>>> Stashed changes
--- src/cache.h
+++ src/cache.h
@@ -1,70 +1,78 @@
 #ifndef __CACHE_H__
 #define __CACHE_H__
 
-// L1 Cache:
-// index = 14
-// offset = 6
-// tag = 12
-// L1 Instruction Cache has 2 lines per set
-// L1 Data Cache has 4 lines per set
-
-// L2 Cache: Stub
 #include <stdint.h>
-#include <stdio.h>
-#include <stdbool.h>
-#include <stdlib.h>
-#include <math.h>
-#include <string.h>
 
-// Compile-time switches
-//#define DEBUG
-//#define VERBOSE_PRINT_
-#define DEFAULT_VERBOSITY 0
-#define MAX_VERBOSITY 3
-
-int VERBOSITY;
 // Data Structures
 
-enum Command_enum
+/* status bits structure for each line
+ */
+struct status_t
 {
+<<<<<<< Updated upstream
 	COMMAND_READ 		= 0,
 	COMMAND_WRITE 		= 1,
 	COMMAND_IFETCH 		= 2,		// read request from L1
 	COMMAND_INVALIDATE 	= 3,	// comes from L2
 	COMMAND_RESET 		= 8,	
 	COMMAND_PRINT 		= 9
+=======
+    unsigned valid:1;
+    unsigned dirty:1;
+>>>>>>> Stashed changes
 };
-	
-// Line Structure
+
+// Line Structure for every set
 struct line_t
 {
     uint32_t tag;               // cache tag
-    uint8_t status;
-    size_t  data_size;
-    uint8_t data[];	        // line data
+    status_t status;            // dirty and valid bits
+    uint8_t  data[];	        // line data
 };
 
 // Set structure
 struct set_t
 {
+<<<<<<< Updated upstream
     uint32_t *lru;			// LRU bits:
     //int lrusize;			// number of bits in LRU
+=======
+    uint8_t *lru;			// LRU bits:
+    int lrusize;			// number of bits in LRU
+>>>>>>> Stashed changes
 					// Space complexity: N*log2(N)
-    size_t linesize;			// number of lines per set
     struct line_t *line[];		// lines in set
-
 };
 
-// Lower Memory hierarchy callback functions
+/* Lower Memory Level callback functons
+ * read  - get a data line from the lower level
+ * write - write back data to lower level
+ * modified - notifies the lower level cache that this value
+ * has changed from clean to dirty. Set these operators to NULL if they are
+ * not used.
+ */
+
+typedef int op_status_t;
 struct LN_ops_t
 {
-	void (*read)(uint32_t address, size_t length, uint8_t* data);
-	void (*write)(uint32_t address, size_t length, uint8_t* data);
+    op_status_t (*read)(uint32_t address, size_t length, uint8_t* data);
+    op_status_t (*write)(uint32_t address, size_t length, uint8_t* data);
+    op_status_t (*modified)(uint32_t address, size_t length, uint8_t* data);
 };
 
+struct cache_params
+{
+    size_t associativity;
+    size_t line_size;
+    size_t index_size;
+};
 
+struct cache_statistics_t
+
+/* Main cache data structure */
 struct cache_t
 {
+<<<<<<< Updated upstream
     // cache data structure
     int setsize;     // number of sets
     int linesize;    // number of lines per set
@@ -75,15 +83,26 @@ struct cache_t
 	c_misses;
     struct LN_ops_t ln_ops;     //callbacks to the lower memory hierarchy
     struct set_t *set[];        //
+=======
+    struct cache_statistics_t stats;
+    struct cache_params_t     params; //size parameters of the cache
+    struct LN_ops_t           ln_ops; //callbacks to the lower memory hierarchy
+    struct set_t              *set[]; //
+>>>>>>> Stashed changes
 };
 
-//**************** cache api *************************************************//
-/* line size  = logn bytes in a line
- * index size = logn sets  
+/***************** Cache API *************************************************/
+/* line size     = number of bytes in a line
+ * index size    = number of sets  
+ * associativity = number of lines per set 
  */
 
-struct cache* cache_new(size_t associativity, size_t line_size, size_t index_size);
+struct cache* cache_new(size_t associativity, 
+			size_t line_size, 
+			size_t index_size,
+			struct LN_ops_t interface);
 void cache_free(struct cache* cacheobj);
+void cache_reset(struct cache* cacheobj);
 
 // input operations
 void cache_readop(struct cache* cacheobj, uint32_t address);
@@ -92,14 +111,6 @@ void cache_invalidate(struct cache* cacheobj, uint32_t address);
 
 // debuging functions
 void cache_print(const struct cache* cacheobj);
-
-
-/*
-// l2cache.c
-// L2 cache functions
-uint32_t *read_stub(struct cache_t *cache, uint32_t address, size_t line_length, uint8_t* data);
-void write_stub(struct cache_t *cache, uint32_t address, size_t line_length, uint8_t* data);
-*/
 
 #endif //__CACHE_H__
 
